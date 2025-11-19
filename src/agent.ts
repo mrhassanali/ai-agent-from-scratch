90e1a83f78dee4ae771f0a22f11652edb3fd11a0
Hassan Ali <73873109+mrhassanali@users.noreply.github.com>
2025-11-20 03:23:56 +0500

    AI Agent Loop

--- src/agent.ts
+++ src/agent.ts
@@ -8,25 +8,34 @@ export const runAgent = async ({ userMessage, tools }: { userMessage: string; to
   await addMessages([{ role: 'user', content: userMessage }])
 
   const loader = showLoader('🤔')
-  const history = await getMessages()
 
-  const response = await runLLM({ messages: history, tools })
+  while (true) {
+    const history = await getMessages()
 
+    const response = await runLLM({ messages: history, tools })
 
-  await addMessages([response])
+    await addMessages([response])
 
-  if (response.tool_calls) {
-    const toolCall = response.tool_calls[0]
+    if(response.content){
+      loader.stop()
+      logMessage(response) // log final message
+      return getMessages();
+    }
 
-    loader.update(`executing: ${toolCall.function.name}`)
+    if (response.tool_calls) {
+      const toolCall = response.tool_calls[0]
 
-    const toolResponse = await runTool(toolCall, userMessage)
-    await saveToolResponse(toolCall.id, toolResponse)
-    loader.update(`done: ${toolCall.function.name}`)
+      logMessage(response)
+
+      loader.update(`executing: ${toolCall.function.name}`)
+
+      const toolResponse = await runTool(toolCall, userMessage)
+      await saveToolResponse(toolCall.id, toolResponse)
+      loader.update(`done: ${toolCall.function.name}`)
+    }
   }
 
-
-  logMessage(response)
-  loader.stop()
-  return getMessages()
+  // logMessage(response)
+  // loader.stop()
+  // return getMessages()
 }
